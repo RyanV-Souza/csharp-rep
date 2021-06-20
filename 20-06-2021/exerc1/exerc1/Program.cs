19b5b05cfdc1ff566b6891d244bf54970db5afa1
RyanV-Souza <ryanvsouza99@gmail.com>
2021-06-20 11:15:46 -0300

    Atividades 20-06-2021

---
+++ 20-06-2021/exerc1/exerc1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace exerc1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome, primeiroNome, sobrenome;
+            int count;
+            
+            Console.Write("Digite seu nome: ");
+            nome = Console.ReadLine();
+            nome = nome.Trim().ToLower();
+
+            string[] nomeSeparado = nome.Split(' ');
+            primeiroNome = nomeSeparado[0];
+            sobrenome = nomeSeparado[nomeSeparado.Length - 1];
+
+            Console.WriteLine("Nome: " + primeiroNome);
+            Console.WriteLine("Sobrenome: " + sobrenome);
+            Console.WriteLine("Substituição: " + nome.Replace("a", "o"));
+
+
+
+
+        }
+    }
+}
---
+++ 20-06-2021/exerc3/exerc3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace exerc3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nomeCompleto;
+
+            Console.Write("Digite seu nome: ");
+            nomeCompleto = Console.ReadLine();
+
+            string[] nomeSeparado = nomeCompleto.Trim().Split(' ');
+
+            Console.WriteLine("Seu nome e-mail é " + nomeSeparado[0].ToLower() + "." + nomeSeparado[nomeSeparado.Length - 1].ToLower() + "@fatec.sp.gov.br");
+        }
+    }
+}
